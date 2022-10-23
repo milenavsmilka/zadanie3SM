21dc39234ee43eba3c5ba2a39352e79a6f14573f
milenavsmilka <104155417+milenavsmilka@users.noreply.github.com>
2022-10-23 12:01:26 +0200

    Delete zad3 directory

--- zad3/ToDoApp/app/src/main/java/com/example/todoapp/TaskListFragment.java
+++
@@ -1,6 +0,0 @@
-package com.example.todoapp;
-
-import androidx.fragment.app.Fragment;
-
-public class TaskListFragment  extends Fragment {
-}
--- zad3/ToDoApp/app/src/main/java/com/example/todoapp/activity/SingleFragmentActivity.java
+++
@@ -1,6 +0,0 @@
-package com.example.todoapp.activity;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public abstract class SingleFragmentActivity extends AppCompatActivity {
-}
